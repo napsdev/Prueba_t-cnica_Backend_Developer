384ac9ee7026a419ed796582bfe95018e70c61f7
Jaime Valencia <jaimevalencia.yepes@hotmail.com>
2021-11-27 17:33:31 -0500

    Configuracion crud productos parte 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rutas del controlador de productos
+Route::resource('productos', ProductoController::class);
